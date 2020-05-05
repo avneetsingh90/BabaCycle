fca1dfcbabf349910226f9afa9ddc8e7173a7297
avneetsingh90 <avneet_singh90@ymail.com>
2020-05-05 14:40:46 +0530

    Updates

---
+++ precache-manifest.c2e173aae869649e074ea61e5b06ea95.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f603dac973a19008bb92d577328e0d8",
+    "url": "/BabaCycle/index.html"
+  },
+  {
+    "revision": "dcd2f6ccb3a3f2234d39",
+    "url": "/BabaCycle/static/css/2.0133fadf.chunk.css"
+  },
+  {
+    "revision": "39a8c6e7c4727a4e128d",
+    "url": "/BabaCycle/static/css/main.88da8758.chunk.css"
+  },
+  {
+    "revision": "dcd2f6ccb3a3f2234d39",
+    "url": "/BabaCycle/static/js/2.43647476.chunk.js"
+  },
+  {
+    "revision": "7a1055b933eafe4ff797137203f06a22",
+    "url": "/BabaCycle/static/js/2.43647476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39a8c6e7c4727a4e128d",
+    "url": "/BabaCycle/static/js/main.bd13c2a9.chunk.js"
+  },
+  {
+    "revision": "76cd1065ecb865a3fa66",
+    "url": "/BabaCycle/static/js/runtime-main.0010f2dc.js"
+  },
+  {
+    "revision": "6ad15a81ec524a9d038e9ddafb9428fc",
+    "url": "/BabaCycle/static/media/User-icon.6ad15a81.png"
+  },
+  {
+    "revision": "668121906cbca7633ec038c230575775",
+    "url": "/BabaCycle/static/media/login-graphics.66812190.svg"
+  },
+  {
+    "revision": "6c2ac7f4ac95a1e993d0d4cf857d836c",
+    "url": "/BabaCycle/static/media/photo-of-women-using-laptops-3178744.6c2ac7f4.jpg"
+  },
+  {
+    "revision": "0ee6d1fbe3c4593e7cb86bbae6fc9348",
+    "url": "/BabaCycle/static/media/silver-and-black-laptop-computer-1229861.0ee6d1fb.jpg"
+  }
+]);
